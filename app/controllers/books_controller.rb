e345ec6109ee269a5a7d524cf34ad0ff0ce56ec0
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-08-13 14:06:36 +0000

    RSpec テスト中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,9 +29,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error: Update failed"
+      render :edit
+    end 
   end
   
   def destroy
